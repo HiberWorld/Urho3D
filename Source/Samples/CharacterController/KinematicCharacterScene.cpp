6e3c3ecf1ea356b857f2aeeaa0c791b49bf522b0
Unknown <chris_albin@outlook.com>
2018-07-02 14:17:32 +0200

    WIP Camera animation
    Failed attempt at adding Camera lerp through animation library.

--- Source/Samples/CharacterController/KinematicCharacterScene.cpp
+++ Source/Samples/CharacterController/KinematicCharacterScene.cpp
@@ -9,17 +9,17 @@ class KinematicCharacterScene : public Application
 public:
 	SharedPtr<Scene> scene_;
 	SharedPtr<Node> cameraNode_;
-	float yaw_ = 0.0f; 
-	float pitch_ = 0.0f; 
+	float yaw_ = 0.0f;
+	float pitch_ = 0.0f;
 	bool firstPerson_ = false;
 
-	Node* charNode_ = nullptr; 
+	Node* charNode_ = nullptr;
 
 	KinematicCharacterScene(Context* context) :
 		Application(context)
 	{
 		SubscribeToEvent(E_BEGINFRAME,
-			URHO3D_HANDLER(KinematicCharacterScene,ApplyGameState)); 
+			URHO3D_HANDLER(KinematicCharacterScene, ApplyGameState));
 
 		KinematicCharacterController::RegisterObject(
 			context_);
@@ -28,22 +28,22 @@ public:
 	void Setup()
 	{
 		engineParameters_[EP_WINDOW_TITLE] = GetTypeName();
-		engineParameters_[EP_FULL_SCREEN] = false; 
-		engineParameters_[EP_WINDOW_WIDTH] = 800; 
-		engineParameters_[EP_WINDOW_HEIGHT] = 600; 
+		engineParameters_[EP_FULL_SCREEN] = false;
+		engineParameters_[EP_WINDOW_WIDTH] = 800;
+		engineParameters_[EP_WINDOW_HEIGHT] = 600;
 		engineParameters_[EP_RESOURCE_PATHS] = "Data;CoreData";
-		input = GetSubsystem<Input>(); 
+		input = GetSubsystem<Input>();
 	}
 
 	void Start()
 	{
-		InitGlobal(context_); 
+		InitGlobal(context_);
 
-		CreateScene(); 
-		SetupViewport(); 
-		SubscribeToEvents(); 
+		CreateScene();
+		SetupViewport();
+		SubscribeToEvents();
 
-		
+
 	}
 
 	void SetupViewport()
@@ -51,7 +51,7 @@ public:
 		Camera* camera = cameraNode_->GetComponent<Camera>();
 		SharedPtr<Viewport> viewport(new Viewport(context_,
 			scene_, camera));
-		renderer->SetViewport(0, viewport); 
+		renderer->SetViewport(0, viewport);
 	}
 
 	void ApplyGameState(StringHash eventType,
@@ -63,16 +63,15 @@ public:
 	void CreateScene()
 	{
 		ResourceCache* cache = GetSubsystem<ResourceCache>();
-		
-		scene_ = new Scene(context_); 
+
+		scene_ = new Scene(context_);
 		SharedPtr<File> levelFile = cache->GetFile
 		("LevelData/Scenes/Level.xml");
 		scene_->LoadXML(*levelFile);
 
-		cameraNode_ = scene_->CreateChild("Camera"); 
+		cameraNode_ = scene_->CreateChild("Camera");
 		cameraNode_->CreateComponent<Camera>();
-		cameraNode_->SetPosition(Vector3(0.0f, 5.0f, 0.0f)); 
-
+		cameraNode_->SetPosition(Vector3(0.0f, 5.0f, 0.0f));
 
 		charNode_ = CreateCharacter();
 	}
@@ -105,22 +104,24 @@ public:
 
 	void HandleUpdate(StringHash eventType, VariantMap& eventData)
 	{
-		using namespace Update; 
+		using namespace Update;
 		float timestep = eventData[P_TIMESTEP].GetFloat();
 
-		IntVector2 mouseMovement = input->GetMouseMove(); 
+		IntVector2 mouseMovement = input->GetMouseMove();
 		yaw_ += MOUSE_SENSITIVITY * mouseMovement.x_;
 		pitch_ += MOUSE_SENSITIVITY * mouseMovement.y_;
-		pitch_ = Clamp(pitch_, -90.0f, 90.0f); 
+		pitch_ = Clamp(pitch_, -90.0f, 90.0f);
 
 		charNode_->SetRotation(Quaternion(0.0f, yaw_, 0.0f));
+
+		CameraMovement(timestep);
 	}
 
 	Node*CreateCharacter()
 	{
-		Node* charNode = scene_->CreateChild(); 
+		Node* charNode = scene_->CreateChild();
 		charNode->SetPosition(Vector3(0.0f, 4.0f, 20.0f));
-		
+
 		Node* adjustNode = charNode->CreateChild("AdjNone");
 		adjustNode->SetRotation(Quaternion(180, Vector3
 		(0.0f, 1.0f, 0.0f)));
@@ -130,15 +131,15 @@ public:
 
 		charObject->SetModel(cache->GetResource<Model>
 			("Models/Mutant/Mutant.mdl"));
-		charObject->SetCastShadows(true); 
+		charObject->SetCastShadows(true);
 
 		charObject->GetSkeleton().GetBone(
 			"Mutant:Head")->animated_ = false;
 
 		KinematicCharacterController* charController =
 			charNode->CreateComponent<KinematicCharacterController>();
-		charController->CreatePhysComponents(1.9f, 0.5f); 
-		return charNode; 
+		charController->CreatePhysComponents(1.9f, 0.5f);
+		return charNode;
 	}
 
 	const float MOUSE_SENSITIVITY = 0.1f;
@@ -152,38 +153,38 @@ public:
 		VariantMap& eventData)
 	{
 		scene_->GetComponent<PhysicsWorld>()->
-			DrawDebugGeometry(true); 
+			DrawDebugGeometry(true);
 	}
-	
+
 	void HandlePostUpdate(StringHash eventType,
 		VariantMap& eventData)
 	{
 		using namespace PostUpdate;
 		float timestep = eventData[P_TIMESTEP].GetFloat();
 
-		Quaternion rot = charNode_->GetRotation();
+		/*Quaternion rot = charNode_->GetRotation();
 		Quaternion dir = rot * Quaternion(pitch_,
-			Vector3::RIGHT); 
-		
-		Node* headNode = charNode_->
-			GetChild("Mutant:Head", true); 
+			Vector3::RIGHT);
 
-		Vector3 aimPoint = charNode_->GetPosition() 
-			+ rot * Vector3(0.0f, 1.9f, 0.0f); 
-		Vector3 rayDir = dir * Vector3::BACK; 
-		float rayRange = 5.0f; 
+		Node* headNode = charNode_->
+			GetChild("Mutant:Head", true);
+
+		Vector3 aimPoint = charNode_->GetPosition()
+			+ rot * Vector3(0.0f, 1.9f, 0.0f);
+		Vector3 rayDir = dir * Vector3::BACK;
+		float rayRange = 5.0f;
 
 		Vector3 firstPersonPos = headNode->GetPosition();
-		Vector3 currentPos = Vector3(firstPersonPos - 
+		Vector3 currentPos = Vector3(firstPersonPos -
 			cameraNode_->GetPosition());
 
-		/*if (firstPerson_)
+		if (firstPerson_)
 		{
 			cameraNode_->SetPosition(headNode->
 				GetWorldPosition() + rot * Vector3
 			(0.0f, 0.15f, 0.2f));
 			cameraNode_->SetRotation(dir);
-		}*/
+		}
 
 		if (firstPerson_)
 		{
@@ -192,8 +193,8 @@ public:
 			{
 				if (Urho3D::Abs(Vector3(
 					cameraNode_->GetPosition()-
-					firstPersonPos).Length() < 
-					currentPos.Length() / 50.0f 
+					firstPersonPos).Length() <
+					currentPos.Length() / 50.0f
 					* timestep))
 				{
 					cameraNode_->SetPosition(
@@ -205,12 +206,12 @@ public:
 				/*cameraNode_->SetPosition(headNode->
 					GetWorldPosition() + rot * Vector3
 					(0.0f, 0.15f, 0.2f));
-				cameraNode_->SetRotation(dir);*/
+				cameraNode_->SetRotation(dir);
 
 				URHO3D_LOGDEBUG("At Head pos");
-				 
+
 				cameraNode_->SetPosition(
-					currentPos / 50.0f * 
+					currentPos / 50.0f *
 					timestep);
 			}
 		}
@@ -219,23 +220,140 @@ public:
 
 		else
 		{
-			PhysicsRaycastResult result; 
+			PhysicsRaycastResult result;
 			scene_->GetComponent<PhysicsWorld>()->
 				RaycastSingle(result, Ray(aimPoint,
-					rayDir), rayRange, 2); 
+					rayDir), rayRange, 2);
 
 			if (result.body_)
 				rayRange = Min(rayRange,
-					result.distance_); 
+					result.distance_);
 			rayRange - Clamp(rayRange, 1.0f, 5.0f);
 
 			cameraNode_->SetPosition(aimPoint +
-				rayDir * rayRange); 
-			cameraNode_->SetRotation(dir); 
+				rayDir * rayRange);
+			cameraNode_->SetRotation(dir);
+
+		}*/
+
+	}
+
+	void CameraMovement(float duration)
+	{
+		Quaternion rot = charNode_->GetRotation();
+		Quaternion dir = rot * Quaternion(pitch_,
+			Vector3::RIGHT);
+
+		Node* headNode = charNode_->
+			GetChild("Mutant:Head", true);
+		//auto camAnim = cameraNode_->GetComponent<AnimationController>();
+
+		Vector3 aimPoint = charNode_->GetPosition()
+			+ rot * Vector3(0.0f, 1.9f, 0.0f);
+		Vector3 rayDir = dir * Vector3::BACK;
+		float rayRange = 5.0f;
+
+		Vector3 firstPersonPos = headNode->GetPosition();
+		Vector3 currentPos = Vector3(firstPersonPos -
+			cameraNode_->GetPosition());
+
+		SharedPtr<ObjectAnimation> cameraAnimation
+		(new ObjectAnimation(context_));
+
+		SharedPtr<ValueAnimation> toThirdPersonAnimation
+		(new ValueAnimation(context_));
+
+		SharedPtr<ValueAnimation> toFirstPersonAnimation
+		(new ValueAnimation(context_));
+
+		toThirdPersonAnimation->SetInterpolationMethod(IM_LINEAR);
+		toThirdPersonAnimation->SetSplineTension(0.7f);
+
+		toFirstPersonAnimation->SetInterpolationMethod(IM_LINEAR);
+		toFirstPersonAnimation->SetSplineTension(0.7f);
+
+
+		if (firstPerson_)
+		{
+			Vector3 firstPersonPos = headNode->GetPosition();
+			
+			toFirstPersonAnimation->SetKeyFrame(0.0f, Vector3(0.0f, 5.0f, 0.0f));
+			toFirstPersonAnimation->SetKeyFrame(4.0f, Vector3(headNode->
+			GetPosition()));
+
+			cameraAnimation->AddAttributeAnimation
+			("Position", toFirstPersonAnimation, WM_CLAMP);
+
+			cameraNode_->SetObjectAnimation(cameraAnimation);
+			cameraNode_->SetPosition(
+				firstPersonPos);
+			cameraNode_->SetRotation(dir);
+
+			/*cameraNode_->SetPosition(headNode->
+					GetWorldPosition() + rot * Vector3
+					(0.0f, 0.15f, 0.2f));
+			cameraNode_->SetRotation(dir);*/
+
+			/*if (headNode->GetPosition() !=
+				firstPersonPos)
+			{
+				if (Urho3D::Abs(Vector3(
+					cameraNode_->GetPosition() -
+					firstPersonPos).Length() <
+					currentPos.Length() / 50.0f
+					* duration))
+				{
+					cameraNode_->SetPosition(
+						firstPersonPos);
+
+				}
+
+				else
+				{
+					URHO3D_LOGDEBUG("At Head pos");
+
+					cameraNode_->SetPosition(
+						currentPos / 50.0f *
+						duration);
+
+					/*cameraNode_->SetPosition(headNode->
+					GetWorldPosition() + rot * Vector3
+					(0.0f, 0.15f, 0.2f));
+					cameraNode_->SetRotation(dir);
+				}
+
+			}*/
 
 		}
-		
-	}
-};
+		else
+		{
+			Vector3 firstPersonPos = headNode->GetPosition();
 
-URHO3D_DEFINE_APPLICATION_MAIN(KinematicCharacterScene)
+			toThirdPersonAnimation->SetKeyFrame(0.0f, Vector3(headNode->
+				GetPosition()));
+			toThirdPersonAnimation->SetKeyFrame(4.0f, Vector3(0.0f, 5.0f, 0.0f));
+
+			PhysicsRaycastResult result;
+			scene_->GetComponent<PhysicsWorld>()->
+				RaycastSingle(result, Ray(aimPoint,
+					rayDir), rayRange, 2);
+
+			if (result.body_)
+				rayRange = Min(rayRange,
+					result.distance_);
+			rayRange - Clamp(rayRange, 1.0f, 5.0f);
+
+			cameraAnimation->AddAttributeAnimation
+			("Position", toFirstPersonAnimation, WM_CLAMP);
+
+			cameraNode_->SetObjectAnimation(cameraAnimation);
+
+			cameraNode_->SetPosition(aimPoint +
+				rayDir * rayRange);
+			cameraNode_->SetRotation(dir);
+		}
+	}
+
+};
+		
+		URHO3D_DEFINE_APPLICATION_MAIN(KinematicCharacterScene)
